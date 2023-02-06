733d9ee14226e57a0ef9602b86ef00735351d8a6
Tyler <terrifictyler8@gmail.com>
2023-02-06 02:35:10 -0500

    made navbar reappear when scrolling up

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -2,7 +2,7 @@
 	import authStore from '$lib/stores/authStore';
 	import firebase from 'firebase/compat/app';
 	import { Button } from 'sveltestrap';
-	import Login from './Login.svelte';
+	import { Navbar, NavbarBrand, Nav, NavItem, NavLink } from 'sveltestrap';
 
 	async function logOut() {
 		await firebase.auth().signOut();
@@ -12,50 +12,98 @@
 			firebaseControlled: true
 		});
 	}
+
+	import { onMount, onDestroy } from 'svelte';
+	let atTop = true;
+	let scrolledUp = false;
+	onMount(() => {
+		var previousScroll = window.pageYOffset;
+		window.onscroll = () => {
+			if (window.pageYOffset < previousScroll) {
+				scrolledUp = true;
+			} else {
+				scrolledUp = false;
+			}
+
+			if (window.pageYOffset === 0) {
+				atTop = true;
+				scrolledUp = false;
+			} else if (window.pageYOffset > 74) {
+				atTop = false;
+			}
+
+			previousScroll = window.pageYOffset;
+		};
+
+		return () => {
+            window.onscroll = () => {};
+        };
+	});
 </script>
 
-<nav id="mainNav" class="navbar navbar-light navbar-expand-lg fixed-top">
-	<div class="container">
-		<a class="navbar-brand" href="#top">TyBlog</a><button
-			class="navbar-toggler"
-			data-bs-toggle="collapse"
-			data-bs-target="#navbarResponsive"
-			aria-controls="navbarResponsive"
-			aria-expanded="false"
-			aria-label="Toggle navigation"><i class="fa fa-bars" /></button
-		>
-		<div id="navbarResponsive" class="collapse navbar-collapse">
-			<ul class="navbar-nav ms-auto">
-				<li class="nav-item"><a class="nav-link" href="/">Home</a></li>
-				<li class="nav-item"><a class="nav-link" href="/about">About</a></li>
-				<li class="nav-item"><a class="nav-link" href="/contact">Contact</a></li>
-				<li class="nav-item"><a class="nav-link" href="/post">Blog</a></li>
-				<li class="nav-item">
-					{#if $authStore.user}
-						<Button outline on:click={logOut} color="light" class="m-2">Logout</Button>
-					{:else}
-						<a class="nav-link" href="/auth/login">Login</a>
-					{/if}
-				</li>
-			</ul>
-		</div>
+<Navbar
+	id="mainNav"
+	class="navbar-light fixed-top {atTop ? '' : 'navbar-is-fixed'} {scrolledUp ? 'navbar-is-visible' : ''}"
+	light
+	expand="md"
+	container
+>
+	<NavbarBrand href="/">TyBlog</NavbarBrand>
+	<button
+		class="navbar-toggler"
+		data-bs-toggle="collapse"
+		data-bs-target="#navbarResponsive"
+		aria-controls="navbarResponsive"
+		aria-expanded="false"
+		aria-label="Toggle navigation"
+		><i class="fa fa-bars" />
+	</button>
+	<div id="navbarResponsive" class="collapse navbar-collapse">
+		<Nav class="ms-auto" navbar>
+			<NavItem><NavLink href="/">Home</NavLink></NavItem>
+			<NavItem><NavLink href="/about">About</NavLink></NavItem>
+			<NavItem><NavLink href="/contact">Contact</NavLink></NavItem>
+			<NavItem><NavLink href="/post">Blog</NavLink></NavItem>
+			<NavItem>
+				{#if $authStore.user}
+					<Button outline on:click={logOut} color="light" class="m-2">Logout</Button>
+				{:else}
+					<NavLink href="/auth/login">Login</NavLink>
+				{/if}
+			</NavItem>
+		</Nav>
 	</div>
-</nav>
+</Navbar>
 
 <style>
-	nav.navbar .navbar-brand {
-		font-family: var(--font-mono);
+	:global(#mainNav) {
+		position: absolute;
+		transition: background-color 0.2s ease;
+		transform: translate3d(0, 0, 0);
+	}
+
+	:global(#mainNav.navbar-is-visible) {
+		transform: translate3d(0, 100%, 0);
+	}
+
+	:global(#mainNav.navbar-is-fixed) {
+		position: fixed;
+		top: -74px;
+		transition: transform 0.2s ease;
+		background-color: var(--color-bg-0);
+	}
+
+	:global(.navbar .navbar-brand) {
 		font-weight: 800;
 		font-size: 2rem;
 		color: white;
 	}
 
-	nav.navbar .nav-item {
+	:global(.navbar .nav-item) {
 		padding-left: 2rem;
 	}
 
-	nav.navbar .nav-link {
-		font-family: var(--font-mono);
+	:global(.navbar .nav-link) {
 		font-weight: 600;
 		font-size: 1.5rem;
 		color: white;
